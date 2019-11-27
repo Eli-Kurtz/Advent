8d62581a060020ef369eb4f516e5d4a1ad1ca483
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-11-27 15:11:52 -0800

    trying the day three problem in python. I believe the problem is how I address my array, but some bits work.

---
+++ 2016_three.py
@@ -0,0 +1,39 @@
+
+
+
+def read (i):
+
+	array = []	
+
+	f = open("2016_three.txt", "r")
+
+	triangle = f.readlines()
+							#https://www.geeksforgeeks.org/reading-writing-text-files-python/
+	array = triangle[i].split(" ")		
+
+	a = (array[2])
+	b = (array[4])
+	c = (array[6])
+
+	total = int(a) + int(b) + int(c)
+
+	a = int(a)
+	b = int(b)
+	c = int(c)
+
+	print(total)
+	
+	count = 0
+
+	if (a + b) > c and (b + c) > a and (c + a) > b:
+		count = 1
+
+	if count == 1:
+		print("this is a triangle")
+
+	print(count)
+
+	read(i+1)
+
+read (0)
+
