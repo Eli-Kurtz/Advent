b47f3ea16853e9bd76bb6ea788cc772efd044b16
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-11-29 10:54:56 -0800

    its working but the counter is incorrect because I am not incrementing by 3.

--- 2016_three.c
+++ 2016_three.c
@@ -7,6 +7,30 @@
 
 
 
+int triangulate (char x[], char y[], char z[], int *n2) {
+
+	int side1 = atoi(x);
+	int side2 = atoi(y);
+	int side3 = atoi(z);
+	int count = 0;
+
+	if (side1 < side2 + side3) {
+		count += 1;}
+	if (side2 < side1 + side3) {
+		count += 1;}
+	if (side3 < side1 + side2) {
+		count += 1;}
+
+	if (count == 3){
+		(*n2)++;}
+
+
+return 0;
+}
+
+
+
+
 void reader (FILE *fp, int *n, char lines[][LEN_STR]) {
 
 	char buffer[LEN_STR] = {'\0'};
@@ -16,12 +40,12 @@ void reader (FILE *fp, int *n, char lines[][LEN_STR]) {
 
 //	sscanf (buffer, "%s", test);
 
-//	sscanf (buffer, "%s %s %s", lines[*n], lines[*n+1], lines[*n+2]);
+	sscanf (buffer, "%s %s %s", lines[*n], lines[*n+1], lines[*n+2]);
 
 //https://www.tutorialspoint.com/c_standard_library/c_function_sscanf.htm
 //This appears to be part of the problem. I cannot use fscanf or scanf because I am reading from a 1D array, not a file or i/o.
 
-	strncpy (lines[*n], buffer, LEN_STR);
+//	strncpy (lines[*n], buffer, LEN_STR);
 
 // unsure if I need the third argument, LEN_STR. gcc says I do, but still seg fault.
 
@@ -45,7 +69,7 @@ void reader (FILE *fp, int *n, char lines[][LEN_STR]) {
 int main () {
 
 	FILE* fp;
-	int n;
+	int n, n2;
 	char (*temp)[LEN_STR] = malloc( NUM_STR * sizeof(char) * LEN_STR);
 
 	fp = fopen( "2016_three.txt", "r");
@@ -58,20 +82,18 @@ int main () {
 
 		strncpy( keeper[i], temp[i], LEN_STR);
 
-	}
+//		printf("\n%s\n", keeper[i]);
 
+	}	
+
+	for (int i=0; i < n; i++) {
 	
-	printf("%s", keeper[0]);	
+		triangulate(keeper[i], keeper[i+1], keeper[i+2], &n2);
+		printf("\n%d\n", n2);
+	}
 
 	return 0;
 }
-
-
-
-
-
-
-
 
 
 
