56576c1f6008971f842c93f47bcee6b5fb33538e
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-12-01 21:00:29 -0800

    this works now

--- 2019_one.c
+++ 2019_one.c
@@ -14,16 +14,18 @@
 #define LEN_STR 100
 
 
-int math_it (char input[]) {
+int math_it (char input[], int* fuel_total) {
     
     float mass = atof(input);
     //https://www.tutorialspoint.com/c_standard_library/c_function_atof.htm
-    int round = floor(mass / 3);
+    int fuel = floor((mass / 3) - 2);
     //https://fresh2refresh.com/c-programming/c-arithmetic-functions/c-floor-function/
     //printf("the input mass is %f", mass);
     //printf("\n%d\n", round);
+    (*fuel_total) = *fuel_total+fuel;
+    
 
-    return 0;
+    return 1;
     
 }
 
@@ -44,7 +46,7 @@ void reader (FILE *fp, int *n, char lines[][LEN_STR]) {
 int main () {
     
     FILE* fp;
-    int n;
+    int n, fuel_total;
     char (*temp)[LEN_STR] = malloc( NUM_STR * sizeof(char) * LEN_STR);
     
     fp = fopen( "2019_one.txt", "r");
@@ -61,7 +63,9 @@ int main () {
     
     for (int i=0; i < n; i++) {
         
-        math_it(keeper[i]);
+        math_it(keeper[i], &fuel_total);
+        printf("\n%d\n", fuel_total);
+        
     }
     
     free(temp);
