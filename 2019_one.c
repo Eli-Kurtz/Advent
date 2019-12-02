ba1fb0347636319fcdf918a44a820b0f0869a99f
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-12-02 13:32:16 -0800

    commiting before I delete a lot of stuff and try again.

--- 2019_one.c
+++ 2019_one.c
@@ -13,24 +13,30 @@
 #define NUM_STR 10000
 #define LEN_STR 100
 
+
+int all_of_it (int init_fuel){
+    int x;
+    
+    while (init_fuel > 0) {
+        
+        printf("\n%d\n", init_fuel);
+        init_fuel = (floor(init_fuel / 3) - 2);
+        
+    }
+    
+    return 0;
+}
+
+
 int math_it (char input[], int* fuel_total) {
     
-    int final_individual_fuel;
     float mass = atof(input);
     //https://www.tutorialspoint.com/c_standard_library/c_function_atof.htm
     int fuel = floor((mass / 3) - 2);
     //https://fresh2refresh.com/c-programming/c-arithmetic-functions/c-floor-function/
     //printf("the input mass is %f", mass);
     //printf("\n%d\n", round);
-    //(*fuel_total) = *fuel_total+fuel;
-    while (1) {
-        
-        final_individual_fuel = (floor(fuel / 3) - 2);
-        printf("%d", final_individual_fuel);
-        if (final_individual_fuel < 0) {
-            break;
-        }
-    }
+    (*fuel_total) = *fuel_total+fuel;
     
     return 0;
     
@@ -55,12 +61,12 @@ int main () {
     FILE* fp;
     int n, fuel_total;
     char (*temp)[LEN_STR] = malloc( NUM_STR * sizeof(char) * LEN_STR);
-    
     fp = fopen( "2019_one.txt", "r");
     reader( fp, &n, temp);
     fclose (fp);
 
     char keeper[n][LEN_STR];
+    int stored_fuel_values[n];
     
     for (int i=0; i < n; i++) {
         
@@ -70,10 +76,20 @@ int main () {
     for (int i=0; i < n; i++) {
         
         math_it(keeper[i], &fuel_total);
-        printf("\n%d\n", fuel_total);
+        stored_fuel_values[i] = fuel_total;
+        //printf("\n%d\n", fuel_total);
+        //printf(" stored value is %d\n", stored_fuel_values[i]);
         
     }
     
+    for (int i=0; i < n; i++) {
+        
+        //printf("the first init fuel is %d", stored_fuel_values[i]);
+        all_of_it(stored_fuel_values[i]);
+        
+    }
+    
+    printf("there are %d numbers", n);
     free(temp);
     
     return 0;
