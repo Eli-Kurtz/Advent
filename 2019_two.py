6d288076b57dbe0e895e0fef6a88da68b1f138da
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-12-01 22:41:08 -0800

    jst beginning, reading the file using syntax I wrote from the 2015 part

---
+++ 2019_two.py
@@ -0,0 +1,22 @@
+
+
+def read ():
+
+	array = []
+
+	f = open("2019_two.txt", "r")
+
+	while True:
+		op_code = f.readlines()
+		array.append(op_code)
+		if not op_code:
+			break
+
+	print(array)
+
+	
+
+
+
+
+read()
