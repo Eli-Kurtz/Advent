6b706d05ef69741e1e3375b8e9f4fe2c51f54e1f
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-12-03 23:56:51 -0800

    kind of working? I think i need the doubles loop to start anywhere in the list

--- 2019_four.py
+++ 2019_four.py
@@ -1,22 +1,33 @@
-
-def remove_decrease (array, n):
-
-    clean_array = []
+def doubles(array, n):
     x = 0
-    y = 1
-    z = 0
+    clean_array = []
+    a = 0
+    b = 1
 
-    for i in array:
-       # print(array[x])
-        while y < 6:
-            if int(array[x][z]) <= int(array[x][y]):
-                print(array[x])
-                clean_array.append(array[x])
-                z += 1
-                y += 1
+    while x < n:
+        if array[x][a] == array[x][b]:
+            a += 1
+            b += 1
+            clean_array.append(array[x])
+            print("hello")
+            if b == 6:
+                break
+        x += 1
 
-    print(clean_array)
+    return clean_array
 
+
+def remove_decrease(array, n):
+
+    x = 0
+    clean_array = []
+    while x < n:
+        if array[x][0] <= array[x][1] and array[x][1] <= array[x][2] and array[x][2] <= array[x][3] and \
+        array[x][3] <= array[x][4] and array[x][4] <= array[x][5]:
+            clean_array.append(array[x])
+        x += 1
+
+    return(clean_array)
 
 
 def array_it ():
@@ -36,10 +47,10 @@ def array_it ():
         i += 1
         a += 1
 
+    possible_password = remove_decrease(possible_password, password_range)
+    possible_password = doubles(possible_password, password_range)
+
     print(possible_password)
-
-    remove_decrease(possible_password, password_range)
-
 
 array_it()
 
