7dfe70d5c1fb720cc25d5aae3175823713f4aee7
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-12-13 09:22:05 -0800

    working on the different modes

---
+++ horner.c
@@ -0,0 +1,8 @@
+//
+//  horner.c
+//  
+//
+//  Created by Eli Kurtz on 12/12/19.
+//
+
+#include <stdio.h>
--- intcode_5.py
+++ intcode_5.py
@@ -1,4 +1,46 @@
 
+def mult_mode(opcode_array, program, ip):
+
+    value_1 = program[ip + 1]
+
+def addition_mode(opcode_array, program, ip):
+
+    pos_out = program[ip + 1]
+    value_1 = program[pos_out]
+
+#unsure about the value of having different modules for each opcode.
+
+def mode(opcode, program, ip):
+
+    opcode_array = list(map(int, str(opcode)))
+
+    length = len(opcode_array)
+    if length == 2:
+        opcode_array.insert(0, 0)
+        opcode_array.insert(0, 0)
+        opcode_array.insert(0, 0)
+
+    elif length == 3:
+        opcode_array.insert(0, 0)
+        opcode_array.insert(0, 0)
+
+    else:
+        print(" FAIL")
+        return
+
+    if opcode == 99:
+        print("program terminated at ip ", ip)
+        return 1
+
+    if opcode_array[-1] == 1:
+        addition_mode(opcode_array, program, ip)
+
+    elif opcode_array[-1] == 2:
+
+
+    return
+
+
 def compute(program, length):
 
     ip = 0
@@ -22,16 +64,30 @@ def compute(program, length):
             ip += 4
             print("ip is ", ip)
 
+        elif program[ip] == 3:
+            user_i = input("enter a number to store at position ")
+            print(program[ip+1])
+            pos_out = program[ip+1]
+            program[pos_out] = int(user_i)
+            ip += 2
+
+        elif program[ip] == 4:
+            pos_out = program[ip+1]
+            print(program[pos_out])
+            ip += 2
+
         else:
-            print("the integer pointer at time of death is", ip)
-            print("program died here at the value",  program[ip])
-            break
+            mode(program[ip], program, ip)
+#            print("\nthe integer pointer at time of death is", ip)
+#            print("\nprogram died here at the value",  program[ip])
+#            break
+            ip += 4
 
     print(program)
 
 
 def reader():
-    with open("2019_five_1.txt") as fo:
+    with open("2019_five.txt") as fo:
         line = fo.readline()
 
     program = [int(s) for s in line.rstrip().split(sep=",")]
