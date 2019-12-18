b4b625b4ef67675df53cdadb3e6ab70177f6f9d4
Eli Kurtz <elikurtz@Elis-MacBook-Air.local>
2019-12-17 17:31:46 -0800

    think I just figured out how to go to the end of one branch

--- 2019_six.py
+++ 2019_six.py
@@ -1,12 +1,33 @@
 
+
 def reader():
     with open("2019_six.txt") as fo:
         line = fo.readlines()
+
     dictionary = dict()
+
+    list_orb = []
+
     for _ in line:
-        orbital = _.split(sep=")")
-        dictionary[orbital[0]] = orbital[1]
-        #https://stackoverflow.com/questions/26263682/python-add-to-dictionary-loop
+        orbital = _.strip().split(sep=")")
+        list_orb.append(orbital)
+
+    x = 0
+
+    for i in list_orb:
+        parent = i[0]
+        child = i[1]
+        if parent not in dictionary:
+            dictionary[parent] = []
+        value = dictionary[parent]
+        value.append(child)
+
+    limb = []
+
+    key = 'COM'
+    while 1:
+        print(dictionary[key])
+        key = dictionary[key][0]
 
 
 reader()
